19764ea543f58309fccd9cd978a7643bdbea62db
rakeshravi796 <rakesh.ravi@minfytech.com>
2025-05-12 15:13:04 +0530

    Improve app

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, redirect, url_for, request , render_template
-app = Flask(__name__)
+app = Flask(__name__,template_folder='template')
 @app.route('/')
 def home():
-    return "<h1> My web application is working</h1>"
+    return render_template("index.html")
+
+@app.route('/add' , methods = ['POST'])
+def adder():
+    numbers = [int(x) for x in request.form.values()]
+    return render_template("index.html" , adder_answer = f"{sum(numbers)}")
 
 if __name__ == '__main__':
    app.run(debug=True, host='0.0.0.0', port=5000)
